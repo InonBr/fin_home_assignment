c1104161b61e7d08b6aa5dc32cdf9d39d1d38332
Inon Bercu <inonbr@gmail.com>
2023-04-04 11:28:08 +0300

    created docker-compose image for our server

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,10 +5,11 @@ dotenv.config();
 export const port = process.env.PORT ? process.env.PORT : "5000";
 export const token = process.env.TOKEN ? process.env.TOKEN : "";
 
+export const endpoint = process.env.DOCKER
+  ? process.env.DOCKER_DYNAMODB_ENDPOINT
+  : process.env.DYNAMODB_ENDPOINT;
+
 export const region = process.env.REGION ? process.env.REGION : "";
-export const endpoint = process.env.DYNAMODB_ENDPOINT
-  ? process.env.DYNAMODB_ENDPOINT
-  : "";
 export const accessKeyId = process.env.ACCESS_KEY_ID
   ? process.env.ACCESS_KEY_ID
   : "";
@@ -18,6 +19,6 @@ export const secretAccessKey = process.env.SECRET_ACCESS_KEY
 
 (() => {
   if (!token || !region || !endpoint || !accessKeyId || !secretAccessKey) {
-    throw new Error("environment variables was not provided");
+    throw new Error("environment variables were not provided");
   }
 })();
